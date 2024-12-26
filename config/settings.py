173bbe9a660b44d92ff986d32b194e73cc382651
Winter <87506748+winterfigueiredo@users.noreply.github.com>
2024-12-26 20:29:00 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
