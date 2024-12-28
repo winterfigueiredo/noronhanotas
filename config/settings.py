4bf532644e29f807c23cff29461a7a702dcacdb5
Winter Figueiredo <winterfigueiredo@gmail.com>
2024-12-28 10:21:40 -0300

    versao com clientes na home

--- config/settings.py
+++ config/settings.py
@@ -79,25 +79,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-#Migrar a base de dados para postgress
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'dbnoronhanotas',
-#         'USER': os.getenv("POSTGRES_USER"),
-#         'PASSWORD': os.getenv("POSTGRES_PASSWORD"), 
-#         'HOST': os.getenv("POSTGRES_HOST"),
-#         'PORT': 5432,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-# 
+#Migrar a base de dados para postgress
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbnoronhanotas',
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"), 
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': 5432,
+    }
+}
+
 
 
 # Password validation
